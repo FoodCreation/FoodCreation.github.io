1d28707d0ca7feae89d48e13a94bc6d6674028ec
cocolemon619 <torikuto.66@gmal.com>
2024-01-28 19:55:11 +0900

    Headerの作成

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,20 +1,16 @@
 import React from "react";
 import HeaderText from "./HeaderText";
+import HeaderNavbar from "./HeaderNavbar";
 
-export default function Header(){
+type HeaderProps = {
+    title: string
+}
+
+export default function Header({ title }: HeaderProps) {
     return (
-        <div className="navbar bg-base-100">
-            <div className="flex-none">
-                <button className="btn btn-square btn-ghost">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-5 h-5 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
-                </button>
-            </div>
-            <HeaderText title="foocre"></HeaderText>
-            <div className="flex-none">
-                <button className="btn btn-square btn-ghost">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-5 h-5 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"></path></svg>
-                </button>
-            </div>
+        <div className="navbar shadow bg-transparent bg-black bg-opacity-10 fixed top-0 z-10">
+            <HeaderText title={title}></HeaderText>
+            <HeaderNavbar></HeaderNavbar>
         </div>
     )
 }
--- src/components/Header/HeaderText.tsx
+++ src/components/Header/HeaderText.tsx
@@ -6,9 +6,9 @@ type HeaderProps = {
 
 export default function HeaderText({title}: HeaderProps){
     return (
-        <div className="navbar bg-base-100">
+        <div className="navbar">
             <div className="flex-1">
-                <a className="btn btn-ghost text-xl">{title}</a>
+                <a className="text-2xl font-bold pl-3" href="/test"> {title}</a>
             </div>
         </div>
     )
