e76aedad8c1567f6e6c42efe791421de91fe8c9b
zawataku <c1254976@st.kanazawa-it.ac.jp>
2024-01-31 22:57:46 +0900

    スライドショーの修正

--- src/components/slider.tsx
+++ src/components/slider.tsx
@@ -1,6 +1,4 @@
 import { Swiper, SwiperSlide } from 'swiper/react'
-import { Navigation } from 'swiper/modules'
-import { Pagination } from 'swiper/modules'
 import { Autoplay } from 'swiper/modules'
 
 import 'swiper/css/navigation'
@@ -9,17 +7,43 @@ import 'swiper/css'
 
 export default function Slider() {
     return (
-        <Swiper
-            className="slider"
-            modules={[Navigation, Pagination, Autoplay]}
-            pagination
-            loop={true}
-            autoplay={{ delay: 4000 }}
-        >
-            <SwiperSlide><img src="/img/desktop/RPT3534.jpg" /></SwiperSlide>
-            <SwiperSlide><img src="/img/desktop/PXL0832.jpg" /></SwiperSlide>
-            <SwiperSlide><img src="/img/desktop/NIJV9793.jpg" /></SwiperSlide>
-            <SwiperSlide><img src="/img/desktop/IMG9248.jpg" /></SwiperSlide>
-        </Swiper>
+        <div className='slider-swiper'>
+
+            {/* ここからデスクトップ向け表示 */}
+            <Swiper
+                className="
+            [&_.swiper-wrapper]:hidden
+            [&_.swiper-wrapper]:lg:flex
+            "
+                modules={[Autoplay]}
+                loop={true}
+                autoplay={{ delay: 4000 }}
+            >
+                <SwiperSlide><img src="/img/desktop/RPT3534.jpg" /></SwiperSlide>
+                <SwiperSlide><img src="/img/desktop/PXL0832.jpg" /></SwiperSlide>
+                <SwiperSlide><img src="/img/desktop/NIJV9793.jpg" /></SwiperSlide>
+                <SwiperSlide><img src="/img/desktop/IMG9248.jpg" /></SwiperSlide>
+            </Swiper>
+            {/* ここまで */}
+
+            {/* ここからモバイル向け表示 */}
+            <Swiper
+                className="
+            [&_.swiper-wrapper]:flex
+            [&_.swiper-wrapper]:lg:hidden
+            "
+                modules={[Autoplay]}
+                loop={true}
+                autoplay={{ delay: 4000 }}
+            >
+                <SwiperSlide><img src="/img/mobile/RPT3534_M.jpg" /></SwiperSlide>
+                <SwiperSlide><img src="/img/mobile/PXL0832_M.jpg" /></SwiperSlide>
+                <SwiperSlide><img src="/img/mobile/NIJV9793_M.jpg" /></SwiperSlide>
+                <SwiperSlide><img src="/img/mobile/IMG9248_M.jpg" /></SwiperSlide>
+            </Swiper>
+            {/* ここまで */}
+
+        </div>
+
     );
 };
