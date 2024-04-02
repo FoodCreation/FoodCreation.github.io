61790a27c088171e4425d4e9f80ee709960f48ab
zawataku <c1254976@st.kanazawa-it.ac.jp>
2024-04-02 19:51:48 +0900

    色々なレイアウト調整

--- src/components/OverView/OverViewText.tsx
+++ src/components/OverView/OverViewText.tsx
@@ -2,11 +2,17 @@ import React from "react";
 
 export default function OverViewText() {
     return (
-        // <div className="flex justify-center card rounded-box place-items-center itmes-center pb-10 w-11/12 mx-auto m-auto pt-3 pb-3">
-        <div className="flex justify-center card rounded-box place-items-center itmes-center  font-semibold pb-10 w-11/12 mx-auto m-auto pt-3 pb-3">
-            「食の未来を考える」をコンセプトに行なわれたアイデアソンから始まり、<br />
-            そこから学生プロジェクトとして活動を行なっています。<br />
-            現在では株式会社ハチバンさんと協働して、班に分かれながら活動しています。
+        <div className="OverViewText">
+            <div className="hidden lg:flex lg:justify-center lg:card lg:rounded-box lg:place-items-center lg:itmes-center lg:text-xl lg:font-semibold lg:w-11/12 lg:mx-auto lg:m-auto lg:pt-3 lg:pb-3">
+                「食の未来を考える」をコンセプトに行なわれたアイデアソンから始まり、<br />
+                そこから学生プロジェクトとして活動を行なっています。<br />
+                現在では株式会社ハチバンさんと協働して、班に分かれながら活動しています。
+            </div>
+            <div className="lg:hidden flex justify-center card rounded-box place-items-center itmes-center font-semibold w-11/12 mx-auto m-auto pt-3 pb-3">
+                「食の未来を考える」をコンセプトに行なわれたアイデアソンから始まり、<br />
+                そこから学生プロジェクトとして活動を行なっています。<br />
+                現在では株式会社ハチバンさんと協働して、班に分かれながら活動しています。
+            </div>
         </div>
     )
 }
---
+++ src/components/OverView/TimeLineLabel.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function TimeLineLabel(){
+    return(
+        <div className="text-4xl ml-9 mb-5 font-bold">
+            プロジェクトの歩み
+        </div>
+    )
+}
