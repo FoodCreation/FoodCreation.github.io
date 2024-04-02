ef839296e8ef7a0c89bc56406172d9c43e389585
cocolemon619 <torikuto.66@gmal.com>
2024-04-02 23:36:45 +0900

    各ラベルのモバイル表示を変更&mbを若干拡大

--- src/components/Teams/TeamsLabel.tsx
+++ src/components/Teams/TeamsLabel.tsx
@@ -2,8 +2,15 @@ import React from "react";
 
 export default function TeamsLabel(){
     return(
-        <div className="text-4xl ml-9 mb-5 font-bold">
-            班紹介
+        <div>
+            {/*デスクトップ画面用*/}
+            <div className="hidden lg:block lg:ml-9 lg:mb-6">
+                <p className="text-4xl font-bold">班紹介</p>
+            </div>
+            {/*モバイル画面用*/}
+            <div className="lg:hidden mb-6">
+                <p className="flex justify-center text-4xl font-bold">班紹介</p>
+            </div>
         </div>
     )
 }
--- src/components/contact/ContactLabel.tsx
+++ src/components/contact/ContactLabel.tsx
@@ -1,9 +1,16 @@
 import React from "react";
 
-export default function ContactLabel(){
-    return(
-        <div id="Contact" className="text-4xl ml-9 mb-3 font-bold">
-            Contact
+export default function ContactLabel() {
+    return (
+        <div>
+            {/*デスクトップ画面用*/}
+            <div id="Contact" className="hidden lg:block lg:ml-9 lg:mb-6">
+                <p className="text-4xl font-bold">Contact</p>
+            </div>
+            {/*モバイル画面用*/}
+            <div id="Contact" className="lg:hidden mb-6">
+                <p className="flex justify-center text-4xl font-bold">Contact</p>
+            </div>
         </div>
     )
 }
