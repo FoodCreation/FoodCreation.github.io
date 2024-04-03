89d9fdf8854765d74da36c4b7865481a7269354f
cocolemon619 <torikuto.66@gmal.com>
2024-04-03 19:06:04 +0900

    Contact周りを修正

--- src/components/contact/ContactText.tsx
+++ src/components/contact/ContactText.tsx
@@ -1,18 +1,23 @@
 import { disconnect } from "process";
 import React from "react";
 
-export default function ContactText() {
+type ContactTextProps = {
+    title:string
+    bodyText:string
+}
+
+export default function ContactText({ title, bodyText }: ContactTextProps) {
     return (
         <div>
             {/*デスクトップ画面用*/}
-            <div className="hidden lg:card lg:bg-slate-300 lg:w-full lg:h-auto lg:p-6">
-                <p className="text-2xl font-bold">連絡先</p>
-                <p className="text-xl">mail: プロジェクトリーダー  富田<br/> c1247965@st.kanazawa-it.ac.jp</p>
+            <div className="hidden lg:card lg:bg-slate-300 lg:w-96 lg:h-auto lg:p-6 lg:mx-2">
+                <p className="text-2xl font-bold">{title}</p>
+                <p className="text-xl">{bodyText}</p>
             </div>
             {/*モバイル画面用*/}
             <div className="lg:hidden card bg-slate-300 w-10/12 h-24 pl-4 mx-auto mb-4">
-                <p className="text-xl font-bold pt-1">連絡先</p>
-                <p className="text-l pt-1">mail: プロジェクトリーダー  富田<br/> c1247965@st.kanazawa-it.ac.jp</p>
+                <p className="text-xl font-bold pt-1">{title}</p>
+                <p className="text-l pt-1">{bodyText}</p>
             </div>
         </div>
     )
