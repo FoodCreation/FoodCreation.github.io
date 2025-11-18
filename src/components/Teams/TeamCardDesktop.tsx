7698afd2498f2bc15b5d28790bb009fce16957be
Takuya-0116 <c1501959@st.kanazawa-it.ac.jp>
2025-11-18 17:46:44 +0900

    appにteamsフォルダと各班のページを作成し、components/Teamsにリンク遷移を実装

--- src/components/Teams/TeamCardDesktop.tsx
+++ src/components/Teams/TeamCardDesktop.tsx
@@ -1,23 +1,25 @@
 import React from "react";
+import Link from "next/link";
 
 type TeamTextProps = {
-    title: string
-    bodytext: string
-    imageUrl: string
-}
+    title: string;
+    bodytext: string;
+    imageUrl: string;
+    linkUrl: string;
+};
 
-export default function TeamCardDesktop({ title, bodytext, imageUrl }: TeamTextProps) {
+export default function TeamCardDesktop({ title, bodytext, imageUrl, linkUrl }: TeamTextProps) {
     return (
         <div className="teamcards w-1/3">
-            {/* ここからデスクトップ向け表示 */}
-            < div className="card shadow-xl image-full mx-auto" >
-                <figure><img src={imageUrl} /></figure>
-                <div className="card-body">
-                    <h1 className="text-4xl text-center font-bold text-slate-100">{title}</h1>
-                    <p className="justify-center items-center flex text-slate-100">{bodytext}</p>
+            <Link href={linkUrl}>
+                <div className="card shadow-xl image-full mx-auto hover:opacity-90 transition">
+                    <figure><img src={imageUrl} alt={title} /></figure>
+                    <div className="card-body">
+                        <h1 className="text-4xl text-center font-bold text-slate-100">{title}</h1>
+                        <p className="justify-center items-center flex text-slate-100">{bodytext}</p>
+                    </div>
                 </div>
-            </div >
-            {/*ここまで*/}
+            </Link>
         </div>
-    )
+    );
 }
---
+++ src/components/Teams/TeamCardMobile.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import Link from "next/link";
+
+type TeamTextProps = {
+  title: string;
+  bodytext: string;
+  imageUrl: string;
+  linkUrl: string;
+};
+
+export default function TeamCardMobile({ title, bodytext, imageUrl, linkUrl }: TeamTextProps) {
+  return (
+    <div className="teamcards">
+      <Link href={linkUrl}>
+        <div className="lg:hidden card shadow-xl image-full mx-5 my-[10px] hover:opacity-90 transition">
+          <figure><img src={imageUrl} alt={title} /></figure>
+          <div className="card-body">
+            <h1 className="text-4xl text-center font-bold text-slate-100">{title}</h1>
+            <p className="justify-center items-center flex text-slate-100">{bodytext}</p>
+          </div>
+        </div>
+      </Link>
+    </div>
+  );
+}
