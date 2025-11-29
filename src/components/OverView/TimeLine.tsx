dddf17fc012b89af586263e6902e30d6a4eb5c8d
SatouYuusei0704 <08kono90su21ba66@gmail.com>
2025-11-30 02:59:32 +0900

    update:必要ではない項目の削除

--- src/components/OverView/TimeLine.tsx
+++ src/components/OverView/TimeLine.tsx
@@ -19,23 +19,8 @@ export default function TimeLine() {
                         </li>
                         <div className="absolute w-4 h-4 bg-orange-500 rounded-full mt-1.5 -start-3 border border-white"></div>
                         <li className="mb-10 ms-4">
-                            <time className="mb-1 text-sm font-normal leading-none text-gray-400">2023.1</time>
-                            <h3 className="text-lg font-semibold text-gray-900">中間発表（企画説明）</h3>
-                        </li>
-                        <div className="absolute w-4 h-4 bg-orange-500 rounded-full mt-1.5 -start-3 border border-white"></div>
-                        <li className="mb-10 ms-4">
-                            <time className="mb-1 text-sm font-normal leading-none text-gray-400">2023.4</time>
-                            <h3 className="text-lg font-semibold text-gray-900">新メンバー加入</h3>
-                        </li>
-                        <div className="absolute w-4 h-4 bg-orange-500 rounded-full mt-1.5 -start-3 border border-white"></div>
-                        <li className="mb-10 ms-4">
                             <time className="mb-1 text-sm font-normal leading-none text-gray-400">2023.11</time>
                             <h3 className="text-lg font-semibold text-gray-900">工大前店オープン<br/>角煮唐麺発売</h3>
-                        </li>
-                        <div className="absolute w-4 h-4 bg-orange-500 rounded-full mt-1.5 -start-3 border border-white"></div>
-                        <li className="mb-10 ms-4">
-                            <time className="mb-1 text-sm font-normal leading-none text-gray-400">2024.4</time>
-                            <h3 className="text-lg font-semibold text-gray-900">新メンバー加入</h3>
                         </li>
                         <div className="absolute w-4 h-4 bg-orange-500 rounded-full mt-1.5 -start-3 border border-white"></div>
                         <li className="mb-10 ms-4">
@@ -43,10 +28,6 @@ export default function TimeLine() {
                             <h3 className="text-lg font-semibold text-gray-900">web班でハッカソン開催</h3>
                         </li>
                         <div className="absolute w-4 h-4 bg-orange-500 rounded-full mt-1.5 -start-3 border border-white"></div>
-                        <li className="mb-10 ms-4">
-                            <time className="mb-1 text-sm font-normal leading-none text-gray-400">2025.4</time>
-                            <h3 className="text-lg font-semibold text-gray-900">新メンバー加入</h3>
-                        </li>
                         <div className="absolute w-4 h-4 bg-orange-500 rounded-full mt-1.5 -start-3 border border-white"></div>
                         <li className="mb-10 ms-4">
                             <time className="mb-1 text-sm font-normal leading-none text-gray-400">2025.10</time>
@@ -84,20 +65,6 @@ export default function TimeLine() {
                                 <div className="timeline-end timeline-box mt-3 font-semibold bg-white border-white">班結成<br />&nbsp;&nbsp;企画</div>
                             </li>
                             <li>
-                                <div className="timeline-start mb-2">2023.1</div>
-                                <div className="timeline-middle">
-                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5 text-orange-600"><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clipRule="evenodd" /></svg>
-                                </div>
-                                <div className="timeline-end timeline-box mt-3 font-semibold bg-white border-white">&nbsp;中間発表<br />(企画説明)</div>
-                            </li>
-                            <li>
-                                <div className="timeline-start mb-2">2023.4</div>
-                                <div className="timeline-middle">
-                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5 text-orange-600"><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clipRule="evenodd" /></svg>
-                                </div>
-                                <div className="timeline-end timeline-box mt-3 font-semibold bg-white border-white">新メンバー加入</div>
-                            </li>
-                            <li>
                                 <div className="timeline-start mb-2">2023.11</div>
                                 <div className="timeline-middle">
                                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5 text-orange-600"><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clipRule="evenodd" /></svg>
@@ -105,25 +72,11 @@ export default function TimeLine() {
                                 <div className="timeline-end timeline-box mt-3 font-semibold bg-white border-white">工大前店オープン<br />&nbsp;角煮唐麺発売</div>
                             </li>
                             <li>
-                                <div className="timeline-start mb-2">2024.4</div>
-                                <div className="timeline-middle">
-                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5 text-orange-600"><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clipRule="evenodd" /></svg>
-                                </div>
-                                <div className="timeline-end timeline-box mt-3 font-semibold bg-white border-white">新メンバー加入</div>
-                            </li>
-                            <li>
                                 <div className="timeline-start mb-2">2024.7</div>
                                 <div className="timeline-middle">
                                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5 text-orange-600"><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clipRule="evenodd" /></svg>
                                 </div>
                                 <div className="timeline-end timeline-box mt-3 font-semibold bg-white border-white">web班でハッカソン開催</div>
-                            </li>
-                            <li>
-                                <div className="timeline-start mb-2">2025.4</div>
-                                <div className="timeline-middle">
-                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5 text-orange-600"><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clipRule="evenodd" /></svg>
-                                </div>
-                                <div className="timeline-end timeline-box mt-3 font-semibold bg-white border-white">新メンバー加入</div>
                             </li>
                             <li>
                                 <div className="timeline-start mb-2">2025.10</div>
