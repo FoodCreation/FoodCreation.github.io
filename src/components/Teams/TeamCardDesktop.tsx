69ed22cb7b4ab94b3ff5374814c0229b5ee86953
Takuya-0116 <c1501959@st.kanazawa-it.ac.jp>
2025-12-03 13:48:52 +0900

    クリック範囲を「詳細はこちら」のみに修正

--- src/components/Teams/TeamCardDesktop.tsx
+++ src/components/Teams/TeamCardDesktop.tsx
@@ -10,16 +10,19 @@ type TeamTextProps = {
 
 export default function TeamCardDesktop({ title, bodytext, imageUrl, linkUrl }: TeamTextProps) {
     return (
-        <div className="teamcards w-1/3">
-            <Link href={linkUrl}>
-                <div className="card shadow-xl image-full mx-auto hover:opacity-90 transition cursor-pointer">
-                    <figure><img src={imageUrl} alt={title} /></figure>
-                    <div className="card-body">
-                        <h1 className="text-4xl text-center font-bold text-slate-100">{title}</h1>
-                        <p className="justify-center items-center flex text-slate-100">{bodytext}</p>
+        <div className="teamcards w-1/3 relative">
+            <div className="card shadow-xl image-full mx-auto hover:opacity-90 transition">
+                <figure><img src={imageUrl} alt={title} /></figure>
+                <div className="card-body">
+                    <h1 className="text-4xl text-center font-bold text-slate-100">{title}</h1>
+                    <p className="justify-center items-center flex text-slate-100">{bodytext}</p>
+                    <div className="absolute bottom-6 right-6">
+                        <Link href={linkUrl} legacyBehavior>
+                            <a className="text-base text-yellow-400 hover:underline cursor-pointer">詳細はこちら</a>
+                        </Link>
                     </div>
                 </div>
-            </Link>
+            </div>
         </div>
     );
 }
--- src/components/Teams/TeamCardMobile.tsx
+++ src/components/Teams/TeamCardMobile.tsx
@@ -2,24 +2,29 @@ import React from "react";
 import Link from "next/link";
 
 type TeamTextProps = {
-    title: string;
-    bodytext: string;
-    imageUrl: string;
-    linkUrl: string;
+  title: string;
+  bodytext: string;
+  imageUrl: string;
+  linkUrl: string;
 };
 
 export default function TeamCardMobile({ title, bodytext, imageUrl, linkUrl }: TeamTextProps) {
-    return (
-        <div className="teamcards">
-            <Link href={linkUrl}>
-                <div className="lg:hidden card shadow-xl image-full mx-5 my-[10px] hover:opacity-90 transition cursor-pointer">
-                    <figure><img src={imageUrl} alt={title} /></figure>
-                    <div className="card-body">
-                        <h1 className="text-4xl text-center font-bold text-slate-100">{title}</h1>
-                        <p className="justify-center items-center flex text-slate-100">{bodytext}</p>
-                    </div>
-                </div>
+  return (
+    <div className="teamcards">
+      <div className="lg:hidden card shadow-xl image-full mx-5 my-[10px] relative">
+        <figure><img src={imageUrl} alt={title} /></figure>
+        <div className="card-body">
+          <h1 className="text-4xl text-center font-bold text-slate-100">{title}</h1>
+          <p className="justify-center items-center flex text-slate-100">{bodytext}</p>
+          <div className="absolute bottom-4 right-4">
+            <Link href={linkUrl} legacyBehavior>
+              <a className="text-base text-yellow-400 hover:underline cursor-pointer">
+                詳細はこちら
+              </a>
             </Link>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
