3c748a5289aee9bff3ff2146044638cc561df451
SatouYuusei0704 <08kono90su21ba66@gmail.com>
2026-03-14 11:54:55 +0900

    fix:文字の修正、ヘッダーのボタンの修正

--- src/components/Header/HeaderNavbar.tsx
+++ src/components/Header/HeaderNavbar.tsx
@@ -9,17 +9,19 @@ export default function HeaderNavbar() {
                     <svg xmlns="http://www.w3.org/2000/svg" fill="#fff" viewBox="0 0 24 24" className="inline-block w-5 h-5 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
                 </div>
                 <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-slate-100 text-slate-900 rounded-box w-56 right-0 font-bold">
-                    <li className="w-1/3"><a href="#About">About</a></li>
-                    <li className="w-1/3"><a href="#Teams">班紹介</a></li>
-                    <li className="w-1/3"><a href="#Contact">Contact</a></li>
+                    <li className="w-1/2"><a href="/#About">About</a></li>
+                    <li className="w-1/2"><a href="/teams/web">web班</a></li>
+                    <li className="w-1/2"><a href="/teams/menu">メニュー班</a></li>
+                    <li className="w-1/2"><a href="/#Contact">Contact</a></li>
                 </ul>
             </div>
             {/* 画面サイズが大きいとき用 */}
-            <div className="hidden lg:bg-opacity-30 lg:flex lg:items-center lg:justify-between lg:pr-[3rem]">
+            <div className="hidden lg:bg-opacity-30 lg:flex lg:items-center lg:justify-between lg:pr-[8rem]">
                 <ul className="list-none flex w-80 text-white">
-                    <li className="w-1/3 pl-3 btn btn-ghost text-lg"><a href="#About">About</a></li>
-                    <li className="w-1/3 pl-3 btn btn-ghost text-lg"><a href="#Teams">班紹介</a></li>
-                    <li className="w-1/3 pl-3 btn btn-ghost text-lg"><a href="#Contact">Contact</a></li>
+                    <li className="w-1/3 pl-3 btn btn-ghost text-lg"><a href="/#About">About</a></li>
+                    <li className="w-1/3 pl-3 btn btn-ghost text-lg"><a href="/teams/web">web班</a></li>
+                    <li className="w-1/3 pl-3 btn btn-ghost text-lg"><a href="/teams/menu">メニュー班</a></li>
+                    <li className="w-1/3 pl-3 btn btn-ghost text-lg"><a href="/#Contact">Contact</a></li>
                 </ul>
             </div>
         </div>
